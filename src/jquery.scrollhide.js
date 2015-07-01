211d2dd1a84cb374a0cfdba17616e0d223203c1c
Viktor <viktor@futurniture.se>
2015-07-01 15:26:07 +0200

    First commit

---
+++ src/jquery.scrollhide.js
@@ -0,0 +1,100 @@
+;(function ( $, window, document, undefined ) {
+
+  'use strict';
+    var pluginName = 'scrollhide';
+
+    function Plugin ( element ) {
+        this.element = element;
+        this._name = pluginName;
+        this.init();
+    }
+
+    $.extend(Plugin.prototype, {
+        init: function () {
+            var down, downY = 0, onceDown = true,
+              y, lastY = 0,
+              upY = 0, onceUp = true,
+              el = $(this.element), elHeight = el.height(), elTop = parseInt(el.css('top'));
+
+            jQuery(window).scroll(function() {
+
+                y = $(this).scrollTop();
+                down = (y > lastY) ? true : false;
+
+                if( y > 100 ){
+
+                  if (down) {
+
+                    if(onceDown){
+                      downY = y;
+                      el
+                        .removeClass('pinned')
+                        .addClass('unpinned')
+                        .css({
+                          'position': 'absolute',
+                          'top': (y + elTop) + 'px'
+                        });
+                      onceDown = false;
+                      onceUp = true;
+                    }
+
+                  }else {
+
+                    if(onceUp) {
+                      upY = y;
+
+                      if( (y - downY - elTop) >= elHeight ) {
+                        el
+                          .removeClass('unpinned')
+                          .css({
+                            'position' : 'absolute',
+                            'top' : (y - elHeight) + 'px'
+                          });
+                      }
+                      onceUp = false;
+                      onceDown = true;
+                    }
+
+                    if( el.hasClass('unpinned') ){
+
+                      if( y <= downY ) {
+                        el
+                          .removeClass('unpinned')
+                          .addClass('pinned')
+                          .css({
+                            'position': 'fixed',
+                            'top': elTop + 'px'
+                          });
+                      }
+                    }
+
+                    if( ((upY - elHeight) - elTop) >= y ){
+
+
+                      el
+                        .removeClass('unpinned')
+                        .addClass('pinned')
+                        .css({
+                          'position': 'fixed',
+                          'top': elTop + 'px'
+                        });
+                    }
+                  }
+
+                }
+
+                lastY = y;
+            });
+
+        }
+    });
+
+    $.fn[ pluginName ] = function ( options ) {
+        return this.each(function() {
+            if ( !$.data( this, 'plugin_' + pluginName ) ) {
+                $.data( this, 'plugin_' + pluginName, new Plugin( this, options ) );
+            }
+        });
+    };
+
+})( jQuery, window, document );
